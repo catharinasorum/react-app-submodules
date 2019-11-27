b220ee7dde8715ef570268a56cef85d14b9edb9e
Catharina <catharina.sorum@outlook.com>
2019-11-27 09:39:05 +0100

    Updates

---
+++ precache-manifest.76cb1c7a5305c5a528ca7a88512d7d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68aa1ae8fa5eee0b89f0cbb03fc9b4bf",
+    "url": "//react-app-submodules/index.html"
+  },
+  {
+    "revision": "a5c0cb90742e294e44aa",
+    "url": "//react-app-submodules/static/css/main.4ee29b56.chunk.css"
+  },
+  {
+    "revision": "f6c546822b5cd118ccf6",
+    "url": "//react-app-submodules/static/js/2.84ebd73b.chunk.js"
+  },
+  {
+    "revision": "a5c0cb90742e294e44aa",
+    "url": "//react-app-submodules/static/js/main.892d22fc.chunk.js"
+  },
+  {
+    "revision": "2327d2d51143bb332711",
+    "url": "//react-app-submodules/static/js/runtime-main.dbbc443a.js"
+  }
+]);
